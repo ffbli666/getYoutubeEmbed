b7344c9bc098b0ef8bf59b8ee8b3cdfc53288d03
Zong-Ying Lyu <ffbli666@gmail.com>
2015-02-09 10:45:49 +0800

    added, play list support

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ include('/libs/youtube.php');
 
 echo getYoutubeEmbed("https://www.youtube.com/watch?v=rnhkLvk7-2k");
 echo "<br>";
-echo getYoutubeEmbed("https://www.youtube.com/watch?v=aVmLYi_e5AM");
+echo getYoutubeEmbed("https://www.youtube.com/watch?v=6KpMYNw3Oz0&list=PLPv96SVEnDc0Ja1b64FXjNpJ4ujJkL1pi&index=5");
 echo "<br>";
-echo getYoutubeEmbed("http://youtu.be/nBcg6SFg5Ag?t=1m29s");
+echo getYoutubeEmbed("http://youtu.be/6KpMYNw3Oz0?list=PLPv96SVEnDc0Ja1b64FXjNpJ4ujJkL1pi");
 echo "<br>";
 echo getYoutubeEmbed("https://www.youtube.com/watch?v=nBcg6SFg5Ag", array( 'rel'      => 1,
                                                                            'controls' => 1,
--- libs/youtube.php
+++ libs/youtube.php
@@ -4,22 +4,27 @@ function getYoutubeID($url) {
         return false;
     }
     $matches = array();
-    if (!preg_match('/^(https?:\/\/)?(www\.youtube\.com\/watch\?v=|youtu.be\/)(?P<youtube_id>[0-9a-z-_]+)/i', $url, $matches)) {
+    if (!preg_match('/^(https?:\/\/)?(www\.youtube\.com\/watch\?v=|youtu.be\/)(?P<id>[0-9a-z-_]+)(?P<list>[&?]list=[0-9a-z-_]*)*/i', $url, $matches)) {
         return false;
     }
-    return $matches['youtube_id'];
+    if ($matches['list']){
+        $matches['list'] = substr($matches['list'], strpos($matches['list'], "=") + 1);        
+    }
+    return $matches;
 }
 
 function getYoutubeEmbed($url, $options = array()) {
-    $youtube_id = getYoutubeID($url);
-    if(!$youtube_id) {
+    $youtube = getYoutubeID($url);
+    if(!$youtube['id']) {
         return false;
     }
 // rel:      影片播放結束後顯示其他推薦影片
 // controls: 顯示播放器控制選項
 // showinfo: 顯示影片名稱和播放器操作
 // private:  啟用隱私權加強保護模式 [?]
-    $def_options = array( 'rel'      => 0,
+    $def_options = array(
+                          'list'     => (isset($youtube['list'])) ? $youtube['list'] : '',
+                          'rel'      => 0,
                           'controls' => 1,
                           'showinfo' => 1,
                           'private'  => 0,
@@ -42,13 +47,18 @@ function getYoutubeEmbed($url, $options = array()) {
     $height = $def_options['height'];
     unset($def_options['height']);
 
+    // if ($youtube['list']) {
+    //     $def_options['list'] = $youtube['list'];
+    // }
+
     $args = array();
     foreach ($def_options as $key=>$value) {
-        $args[] = $key . '=' . $value;
+        if ($value)
+            $args[] = $key . '=' . $value;
     }
     $arg = implode('&amp;', $args);
     $arg = ($arg) ? '?' . $arg : $arg;
-    $full_url = $url . $youtube_id . $arg;
+    $full_url = $url . $youtube['id'] . $arg;
 
     return '<iframe width="'. $width .'" height="' . $height . '" src="' . $full_url . '" frameborder="0" allowfullscreen></iframe>';
 
